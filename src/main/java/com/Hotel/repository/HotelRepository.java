28e69d3ebd5e064ea245660f861c71c95f29e5f1
shashipratapsingh <itpro8577@gmail.com>
2025-04-01 19:46:48 +0530

    again

--- src/main/java/com/Hotel/repository/HotelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Hotel.repository;
-
-import com.Hotel.entity.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HotelRepository extends JpaRepository<Hotel,String> {
-}
